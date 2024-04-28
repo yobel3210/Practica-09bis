db4646a6ed73e8938d8b014f63f9f544c388163e
yobel3210 <doloreszeferinoyobel@gmail.com>
2024-04-27 20:34:08 -0600

    Versión que solicita varios números al usuario e imprime el más grande de todos ellos.

---
+++ prog1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ## ###############################################################
+# prog1.py
+#
+# Author: Mauricio Matamoros
+# License: MIT
+#
+# Finds the largest number of those provided by the user
+#
+# ## ###############################################################
+import sys
+from math import sin
+
+def read_int():
+	try:
+		s = input("int? ")
+		n = int(s)
+		return n
+	except:
+		return None
+
+def main(argv):
+	n = read_int()
+	numbers = []
+	while n:
+		numbers.append(n)
+		n = read_int()
+	print("El elemento más grande es:", max(numbers) )
+
+if __name__ == '__main__':
+	main(sys.argv)
